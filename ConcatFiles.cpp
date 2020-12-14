67446b937a1ce67dd3783d2a0575e6f5671408cf
Viktoriia <viktoryacherednichenko@gmail.com>
2020-12-14 13:13:22 +0200

    fixed

--- ConcatFiles.cpp
+++ ConcatFiles.cpp
@@ -1,99 +1,56 @@
+#include <iostream>
 #include <windows.h>
-#include <stdio.h>
-#include <tchar.h>
 
-int _tmain(int argc, _TCHAR* argv[])
-{     
-    DWORD dwBytesRead, dwBytesWritten, dwPos;
-    BYTE buff[4096] = {};
-      
-    HANDLE hFile = CreateFile(TEXT("res.txt"), 
-        FILE_APPEND_DATA,         
-        FILE_SHARE_READ,          
-        NULL,                     
-        CREATE_ALWAYS,            
-        FILE_ATTRIBUTE_NORMAL,    
-        NULL);                    
-
-    if (hFile == INVALID_HANDLE_VALUE)
-    {
-        printf("Could not open res.txt.");
-        return 0;
+int main(int argc, char** argv)
+{
+    if (argc <= 2) {
+        std::cerr << "Invalid arguments";
+        return -1;
     }
-
-    HANDLE hFile1 = CreateFile(TEXT("C:\\tmp\\ConcatFiles\\One.txt"),
-        GENERIC_READ,
-        0,
+    std::string folderPath = argv[1];
+    std::string resFilePath = folderPath + "\\res.txt";
+    HANDLE resFileHandle = CreateFileA(resFilePath.c_str(),
+        FILE_APPEND_DATA,
+        FILE_SHARE_READ,
         NULL,
-        OPEN_EXISTING,
+        OPEN_ALWAYS,
         FILE_ATTRIBUTE_NORMAL,
         NULL);
 
-    if (hFile1 == INVALID_HANDLE_VALUE)
+    if (resFileHandle == INVALID_HANDLE_VALUE)
     {
-        printf("Could not open One.txt");
+        std::cout << "Could not open file " << resFilePath;
         return 0;
     }
+    for (int i = 2; i < argc; ++i) {
+        std::string filePath = folderPath + "\\" + argv[i];
+        HANDLE file = CreateFileA(filePath.c_str(),
+            GENERIC_READ,
+            0,
+            NULL,
+            OPEN_EXISTING,
+            FILE_ATTRIBUTE_NORMAL,
+            NULL);
 
-    while (ReadFile(hFile1, buff, sizeof(buff), &dwBytesRead, NULL)
-        && dwBytesRead > 0)
-    {
-        dwPos = SetFilePointer(hFile, 0, NULL, FILE_END);
-        LockFile(hFile, dwPos, 0, dwBytesRead, 0);
-        WriteFile(hFile, buff, dwBytesRead, &dwBytesWritten, NULL);
-        UnlockFile(hFile, dwPos, 0, dwBytesRead, 0);
+        if (file == INVALID_HANDLE_VALUE)
+        {
+            std::cout << "Could not open file " << resFilePath;
+            return 0;
+        }
+        char buf[1000] = {};
+        DWORD bytesRes = 0;
+        BOOL res = ReadFile(file, buf, sizeof(buf), &bytesRes, NULL);
+        if (!res) {
+            std::cout << "Could not read file " << resFilePath;
+            return 0;
+        }
+        res = WriteFile(resFileHandle, buf, bytesRes, &bytesRes, NULL);
+        if (!res) {
+            std::cout << "Could not write file " << resFilePath;
+            return 0;
+        }
+        CloseHandle(file);
     }
-    
-    HANDLE hFile2 = CreateFile(TEXT("C:\\tmp\\ConcatFiles\\Two.txt"),
-        GENERIC_READ,
-        0,
-        NULL,
-        OPEN_EXISTING,
-        FILE_ATTRIBUTE_NORMAL,
-        NULL);
-
-    if (hFile2 == INVALID_HANDLE_VALUE)
-    {
-        printf("Could not open Two.txt");
-        return 0;
-    }
-
-    while (ReadFile(hFile2, buff, sizeof(buff), &dwBytesRead, NULL)
-        && dwBytesRead > 0)
-    {
-        dwPos = SetFilePointer(hFile, 0, NULL, FILE_END);
-        LockFile(hFile, dwPos, 0, dwBytesRead, 0);
-        WriteFile(hFile, buff, dwBytesRead, &dwBytesWritten, NULL);
-        UnlockFile(hFile, dwPos, 0, dwBytesRead, 0);
-    }
-
-    HANDLE hFile3 = CreateFile(TEXT("C:\\tmp\\ConcatFiles\\Three.txt"),
-        GENERIC_READ,
-        0,
-        NULL,
-        OPEN_EXISTING,
-        FILE_ATTRIBUTE_NORMAL,
-        NULL);
-
-    if (hFile3 == INVALID_HANDLE_VALUE)
-    {
-        printf("Could not open Three.txt");
-        return 0;
-    }
-
-    while (ReadFile(hFile3, buff, sizeof(buff), &dwBytesRead, NULL)
-        && dwBytesRead > 0)
-    {
-        dwPos = SetFilePointer(hFile, 0, NULL, FILE_END);
-        LockFile(hFile, dwPos, 0, dwBytesRead, 0);
-        WriteFile(hFile, buff, dwBytesRead, &dwBytesWritten, NULL);
-        UnlockFile(hFile, dwPos, 0, dwBytesRead, 0);
-    }
-
-    CloseHandle(hFile);
-    CloseHandle(hFile1);
-    CloseHandle(hFile2);
-    CloseHandle(hFile3);
-
-	return 0;
+    CloseHandle(resFileHandle);
+    return 0;
 }
